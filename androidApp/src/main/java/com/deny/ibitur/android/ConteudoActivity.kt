ed89d5f2618552ddb77e7cfd730c8fd3d125e0d5
Bruno Brito <batioe.std@gmail.com>
2022-02-27 15:07:22 -0300

    Retirei o float action button

--- androidApp/src/main/java/com/deny/ibitur/android/ConteudoActivity.kt
+++ androidApp/src/main/java/com/deny/ibitur/android/ConteudoActivity.kt
@@ -44,10 +44,6 @@ class ConteudoActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarConteudo.toolbar)
 
-        binding.appBarConteudo.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_conteudo)
