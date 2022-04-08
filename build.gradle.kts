db4a561a736fbd07c3ad525e64db2cf4e468b3c2
bruno.std <batioe.std@gmail.com>
2022-04-08 19:21:53 -0300

    Organizando

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.20-RC")
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("com.google.gms:google-services:4.3.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
